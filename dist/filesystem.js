45ac9955179a8ce4b6a5472432975c8d908fb2c6
Gaurav Panchal <panchalgaurav487@gmail.com>
2024-01-29 18:14:36 +0530

    Day3

--- dist/filesystem.js
+++ dist/filesystem.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require('fs');
 //Writing a file
 fs.writeFile('file.txt', "Created a file ", (err) => {
@@ -6,18 +7,49 @@ fs.writeFile('file.txt', "Created a file ", (err) => {
         console.log(err);
     }
 });
-const rename = fs.renameSync('file.txt', 'Renamed.txt');
-console.log(rename);
+// const rename = fs.renameSync('file.txt','Renamed')
+// console.log (rename)
 // //Reading a file
-// fs.readFile("file.txt", 'utf-8', (err, data) => {
+// fs.readFile("file.txt", 'utf-8',(err:any,data:any) =>{
 //     if (err) {
 //         console.log(err);
 //         return;
 //     }
 //     console.log("File Data: \n " + data);
 // });
-// //Deleting a file 
-// fs.unlink('file.txt', (err:any) =>{
-//     if (err)
-//         console.log(err);
-// });
+// // //Deleting a file 
+// // fs.unlink('file.txt', (err:any) =>{
+// //     if (err)
+// //         console.log(err);
+// // });
+//Day3-
+//Readable stream
+// const stream =fs.createReadStream('Renamed.txt', 'utf-8')
+// stream.on('data',(chunk:any)=>{
+//     console.log(chunk)
+// })
+// stream.on('end',() =>console.log('stream finished'))
+//Writable stream
+const stream = fs.createReadStream('Renamed.txt', 'utf-8');
+const writeStream = fs.createWriteStream('text.md', 'utf-8');
+// stream.on('data',(chunk:any)=>{
+//     console.log(chunk)
+//     writeStream.write(chunk)
+// })
+// stream.on('end',() =>{
+//     writeStream.end()
+// })  
+//stream.pipe(writeStream).on('error',(error) => console.log(error)) //pipes
+//  writeStream.on('close',()=> process.stdout.write('file copied'))
+//HTTP//
+const https = require('https');
+const options = {
+    hostname: "",
+    port: "",
+    path: "",
+    method: "GET",
+};
+const request = https.request(options, (res) => {
+    let responsebody = "";
+    res.SetEncoding('utf-8');
+});
--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,3 +1,7 @@
+import { error } from "console";
+import { WriteStream } from "fs";
+import { hostname } from "os";
+
 const fs = require('fs');
 
 //Writing a file
@@ -22,3 +26,45 @@ fs.writeFile('file.txt',"Created a file ",(err:any) =>{
 // //     if (err)
 // //         console.log(err);
 // // });
+
+
+                                                    //Day3-
+//Readable stream
+// const stream =fs.createReadStream('Renamed.txt', 'utf-8')
+// stream.on('data',(chunk:any)=>{
+//     console.log(chunk)
+// })
+
+// stream.on('end',() =>console.log('stream finished'))
+
+//Writable stream
+const stream =fs.createReadStream('Renamed.txt', 'utf-8')
+const writeStream = fs.createWriteStream('text.md','utf-8')
+// stream.on('data',(chunk:any)=>{
+//     console.log(chunk)
+//     writeStream.write(chunk)
+// })
+
+// stream.on('end',() =>{
+//     writeStream.end()
+// })  
+//stream.pipe(writeStream).on('error',(error) => console.log(error)) //pipes
+  //  writeStream.on('close',()=> process.stdout.write('file copied'))
+
+
+                                       //HTTP//
+
+
+ const https = require('https')     
+ const options = {
+    hostname:"",
+    port:"",
+    path:"",
+    method :"GET",
+
+ }       
+
+ const request = https.request(options ,(res:any)=> {
+    let responsebody ="";
+    res.SetEncoding('utf-8'); 
+ })
